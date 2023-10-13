4685979c43fe4de3e765e99908e7f252431b8802
Naman <98084335+namangarg2002@users.noreply.github.com>
2023-10-13 19:20:52 +0530

    Time: 15 ms (79.64%), Space: 32.7 MB (5.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-int bruteForce(vector<int>& nums){
-    for(int i=0; i<nums.size(); i++){
-        int lsum=0;
-        int rsum=0;
+// int bruteForce(vector<int>& nums){
+//     for(int i=0; i<nums.size(); i++){
+//         int lsum=0;
+//         int rsum=0;
 
-        // left sum excluding current index
+//         // left sum excluding current index
 
-        for(int j=0; j<i; j++){
-            lsum += nums[j];
+//         for(int j=0; j<i; j++){
+//             lsum += nums[j];
+//         }
+//         for(int j=i+1; j<nums.size(); j++){
+//             rsum += nums[j];
+//         }
+//         if(lsum==rsum){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+    int greedyApproach(vector<int>& nums){
+        vector<int>lsum(nums.size(), 0);
+        vector<int>rsum(nums.size(), 0);
+
+        for(int i=1; i<nums.size(); i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
         }
-        for(int j=i+1; j<nums.size(); j++){
-            rsum += nums[j];
+        for(int i=nums.size()-2; i>=0; i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
         }
-        if(lsum==rsum){
-            return i;
+        for(int i=0;i<nums.size(); i++){
+            if(lsum[i]==rsum[i]){
+                return i;
+            }
         }
+        return -1;
     }
-    return -1;
-}
+
     int pivotIndex(vector<int>& nums) {
-        int ans = bruteForce(nums);
+        // int ans = bruteForce(nums);
+        int ans = greedyApproach(nums);
         return ans;
     }
 };
