11605090335dbc015b11029b0370ea36dfc0bf82
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-02-03 11:42:53 +0530

    Time: 4 ms (86.02%) | Memory: 8.6 MB (7.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return(s==t) ? true : false;
+        // simple but high complexity approach(Sorting)
+
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return(s==t) ? true : false;
+
+        int freqTable[256] = {0};
+        for(int i=0; i<s.size(); i++){
+            freqTable[s[i]]++;
+        }
+        for(int i=0; i<t.size(); i++){
+            freqTable[t[i]]--;
+        }
+        for(int i=0; i<256; i++){
+            if(freqTable[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
