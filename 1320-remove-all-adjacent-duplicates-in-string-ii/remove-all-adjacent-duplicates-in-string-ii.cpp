d9abe2c027b4dc7f4159153872bbdd715bf93ca9
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-02-15 20:51:28 +0530

    Time: 10 ms (97.44%) | Memory: 12 MB (48.88%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -8,7 +8,7 @@ public:
         }
         return true;
     }
-    string removeDuplicates(string s, int k) {
+    string removeDuplicates2(string s, int k) {
        string ans;
        for(int i=0; i<s.size(); i++){
            char &newch = s[i];
@@ -26,5 +26,24 @@ public:
            }
        }
        return ans;
+    }
+
+    string removeDuplicates(string s, int k) {
+        //    two ppointer approach
+        int i=0, j=0;
+        vector<int>count(s.size());
+        while(j<s.size()){
+            s[i]=s[j];
+            count[i] = 1;
+            if(i > 0 && s[i] == s[i-1]){
+                count[i] += count[i-1];
+            }
+            if(count[i] == k){
+                i-=k;
+            }
+            i++;
+            j++;
+        }
+        return s.substr(0, i);
     }
 };
