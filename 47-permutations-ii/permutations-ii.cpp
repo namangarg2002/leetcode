47ec7db1dcfdfc7d5f0dd19b3396afb252735e18
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-06-18 20:14:52 +0530

    Time: 6 ms (61.88%) | Memory: 14.2 MB (22.38%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -6,8 +6,12 @@ public:
             ans.push_back(nums);
             return;
         }
-
+        unordered_map<int, bool>visited;
         for(int i=start; i<nums.size(); i++){
+            if(visited.find(nums[i]) != visited.end()){
+                continue;
+            }
+            visited[nums[i]]  = true;
             swap(nums[i],nums[start]);
             permuteUniqueHelper(nums, ans, start+1);
             swap(nums[i],nums[start]);
@@ -16,15 +20,15 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>ans;
         permuteUniqueHelper(nums, ans, 0);
-        // push answer in to set
-        set<vector<int>> st;
-        for(auto e:ans){
-            st.insert(e);
-        }
-        ans.clear();
-        for(auto e:st){
-            ans.push_back(e);
-        }
+        // // push answer in to set
+        // set<vector<int>> st;
+        // for(auto e:ans){
+        //     st.insert(e);
+        // }
+        // ans.clear();
+        // for(auto e:st){
+        //     ans.push_back(e);
+        // }
         return ans;
     }
 };
