f9b0fd119ab331a73007a2e95f470c0ff408879f
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-06-18 21:07:18 +0530

    Time: 324 ms (5.15%) | Memory: 7.1 MB (91.79%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void countArrangementHelper(vector<int>&v,int &n, int &ans, int currNum){
+        // base condition
+        if(currNum == n + 1){
+            for(int i=1; i<=n; i++){
+                cout<<v[i]<<" ";
+            }
+            cout<<endl;
+            ans++;
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            if(v[i] == 0 && (currNum % i == 0 || i % currNum == 0)){
+                v[i] = currNum;
+                countArrangementHelper(v, n, ans, currNum+1);
+                v[i] = 0; // backtracking step
+            }
+        }
+    }
+    int countArrangement(int n) {
+        vector<int>v(n+1);
+        int ans = 0;
+        countArrangementHelper(v, n, ans, 1);
+        return ans;
+    }
+};
