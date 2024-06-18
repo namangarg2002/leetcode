25400c69a441a4f72c8c5b9a5b5ffee61a65742f
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-06-18 10:10:39 +0530

    Time: 247 ms (53.49%) | Memory: 69.1 MB (88.54%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,35 +1,62 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, vector<int>& temp, int start, int mid, int end){
-        int i = start, j = mid + 1, k = start;
-        while(i<=mid && j<=end){
-            if(nums[i] <= nums[j]){
-                temp[k++] = nums[i++];
-            }else{
-                temp[k++] = nums[j++];
+    // void merge(vector<int>& nums, vector<int>& temp, int start, int mid, int end){
+    //     int i = start, j = mid + 1, k = start;
+    //     while(i<=mid && j<=end){
+    //         if(nums[i] <= nums[j]){
+    //             temp[k++] = nums[i++];
+    //         }else{
+    //             temp[k++] = nums[j++];
+
+    //         }
+    //     }
+    //     while(i<=mid){
+    //         temp[k++] = nums[i++];
+    //     }
+    //     while(j<=end){
+    //         temp[k++] = nums[j++];
+    //     }
+    //     while(start<=end){
+    //         nums[start] = temp[start];
+    //         start++;
+    //     }
+    // }
+    // void mergeSort(vector<int>& nums, vector<int>& temp, int start, int end){
+    //     if(start >= end) return;
+    //     int mid = (start + end) >> 1;
+    //     mergeSort(nums, temp, start, mid);
+    //     mergeSort(nums, temp, mid+1, end);
+    //     merge(nums, temp, start, mid, end);
+    // }
+    // vector<int> sortArray(vector<int>& nums) {
+    //     vector<int> temp(nums.size(),0);
+    //     mergeSort(nums, temp, 0,nums.size()-1);
+    //     return nums;
+        
+    // }
+    void mergeInPlace(vector<int>& nums, int start, int mid, int end){
+        int total_len = end - start + 1;
+        int gap = (total_len / 2) + (total_len % 2);
+        while(gap > 0){
+            int i = start, j = i + gap;
+            while(j<=end){
+                if(nums[i]>nums[j]){
+                    swap(nums[i], nums[j]);
+                }
+                i++,j++;
             }
-        }
-        while(i<=mid){
-            temp[k++] = nums[i++];
-        }
-        while(j<=end){
-            temp[k++] = nums[j++];
-        }
-        while(start<=end){
-            nums[start] = temp[start];
-            start++;
+            gap = gap <= 1 ? 0 : (gap / 2) + (gap % 2);
         }
     }
-    void mergeSort(vector<int>& nums, vector<int>& temp, int start, int end){
+    void mergeSort(vector<int>& nums, int start, int end){
         if(start >= end) return;
         int mid = (start + end) >> 1;
-        mergeSort(nums, temp, start, mid);
-        mergeSort(nums, temp, mid+1, end);
-        merge(nums, temp, start, mid, end);
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        mergeInPlace(nums, start, mid, end);
     }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int> temp(nums.size(),0);
-        mergeSort(nums, temp, 0,nums.size()-1);
+        mergeSort(nums, 0,nums.size()-1);
         return nums;
         
     }
