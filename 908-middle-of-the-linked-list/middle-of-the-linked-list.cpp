997fb33dbdd9a9bff5a08e6fda352c042f7593f2
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-07-19 20:10:08 +0530

    Time: 4 ms (34.71%) | Memory: 9.1 MB (9.08%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,24 +10,39 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* head){
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL){
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
+    // int getLength(ListNode* head){
+    //     ListNode* temp = head;
+    //     int count = 0;
+    //     while(temp != NULL){
+    //         count++;
+    //         temp = temp->next;
+    //     }
+    //     return count;
+    // }
     ListNode* middleNode(ListNode* head) {
-        int n = getLength(head);
-        int position = n/2 + 1;
-        int currPos = 1;
-        ListNode *temp = head;
-        while(currPos != position){
-            currPos++;
-            temp = temp->next;
+        // int n = getLength(head);
+        // int position = n/2 + 1;
+        // int currPos = 1;
+        // ListNode *temp = head;
+        // while(currPos != position){
+        //     currPos++;
+        //     temp = temp->next;
+        // }
+        // return temp;
+
+        // Tortoise ALgorithm
+        ListNode* slow = head; 
+        ListNode* fast = head;
+        while(fast != NULL){
+            fast = fast->next; // fast ne yaha tk 1 step chal liya h
+            if(fast != NULL){
+                fast = fast->next;
+                // yaha tk fast ne 2 step chl liye h.
+
+                // ab slow ko bhi chl walo 1 step
+                slow = slow->next;
+            }
         }
-        return temp;
+        return slow;                                    
     }
 };
