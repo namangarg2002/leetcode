c84bdcc36e48daf9729b0f852ae0e869f2da5dfd
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-07-19 17:34:14 +0530

    Time: 4 ms (59.24%) | Memory: 13 MB (8.58%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,15 +10,29 @@
  */
 class Solution {
 public:
+    ListNode* reverseUsingRecursion( ListNode* prev,ListNode* curr){
+        // base case
+        if(curr == NULL){
+            return prev;
+        }
+        // 1 case we have solve next recursion will handle it
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        return reverseUsingRecursion(curr, nextNode);
+
+    }
     ListNode* reverseList(ListNode* head) {
         ListNode *prev = NULL;
         ListNode *curr = head;
-        while(curr != NULL){
-            ListNode *next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        // while(curr != NULL){
+        //     ListNode *next = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+        // return prev;
+
+        return reverseUsingRecursion(prev, curr);
+
     }
 };
