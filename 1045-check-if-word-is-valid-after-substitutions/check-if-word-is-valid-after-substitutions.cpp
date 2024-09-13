c88d3cc4e0502356f22c33870befb2551b0c4a1a
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-09-13 17:42:14 +0530

    Time: 15 ms (42.33%) | Memory: 10.9 MB (35.43%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 0){
-            return true;
+        // **Using recursion** //
+        // if(s.size() == 0){
+        //     return true;
+        // }
+        // int fnd = s.find("abc");
+        // if(fnd != string::npos){
+        //     // found
+        //     string tleft = s.substr(0, fnd);
+        //     string tright = s.substr(fnd + 3, s.size());
+        //     return isValid(tleft + tright);
+        // }
+        // return false;
+        if(s[0] != 'a') return false;
+        stack<char>st;
+        for(char ch: s){
+            if(ch == 'a'){
+                st.push(ch);
+            }else if(ch == 'b'){
+                if(!st.empty() && st.top()=='a'){
+                   st.push('b'); 
+                }else{
+                    return false;
+                }
+            }else{
+                // ch == 'c'
+                if(!st.empty() && st.top() == 'b'){
+                    st.pop();
+                    if(!st.empty() && st.top() == 'a'){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
         }
-        int fnd = s.find("abc");
-        if(fnd != string::npos){
-            // found
-            string tleft = s.substr(0, fnd);
-            string tright = s.substr(fnd + 3, s.size());
-            return isValid(tleft + tright);
-        }
-        return false;
+        return st.empty() ? true : false;
+
     }
 };
