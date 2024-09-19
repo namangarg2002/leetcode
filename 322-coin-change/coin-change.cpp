7cf5f567c2060a8c4e99e8012c729688d77f21ea
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-09-19 11:29:09 +0530

    Time: 42 ms (43.61%) | Memory: 17.7 MB (54.25%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int topdown(vector<int>& coins, int amount, vector<int>& dp){
+        if(amount == 0){
+            return 0;
+        }
+        if(amount < 0){
+            return INT_MAX;
+        }
+
+        if(dp[amount] != -1){
+            return dp[amount];
+        }
+
+        int mini = INT_MAX;
+
+        for(int i=0; i<coins.size(); i++){
+            int ans = topdown(coins, amount-coins[i], dp);
+
+            if(ans != INT_MAX){
+                mini = min(mini, 1+ans);
+            }
+        }
+        dp[amount] = mini;
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, -1);
+        int ans = topdown(coins, amount, dp);
+        if(ans == INT_MAX){
+            return -1;
+        }
+        else{
+            return ans;
+        }
+    }
+};
