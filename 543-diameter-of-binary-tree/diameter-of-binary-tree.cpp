3527f79bd542da7f91680317d2f14e659291bf27
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-11-25 11:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (84.56%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,29 +11,45 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        // base case
-        if(root == NULL){
-            return 0;
-        }
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
+    // int height(TreeNode* root) {
+    //     // base case
+    //     if(root == NULL){
+    //         return 0;
+    //     }
+    //     int leftHeight = height(root->left);
+    //     int rightHeight = height(root->right);
 
-        int height = max(leftHeight, rightHeight) + 1;
+    //     int height = max(leftHeight, rightHeight) + 1;
 
-        return height;
+    //     return height;
+    // }
+
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //     // base case
+    //     if(root == NULL){
+    //         return 0;
+    //     }
+    //     int option1 = diameterOfBinaryTree(root->left);
+    //     int option2 = diameterOfBinaryTree(root->right);
+    //     int option3 = height(root->left) + height(root->right);
+    //     int diameter = max(option1, max(option2, option3));
+
+    //     return diameter;
+    // }
+
+    int D = 0;
+    int height(TreeNode* root){
+        if(!root) return 0;
+
+        int left = height(root->left);
+        int right = height(root->right);
+        // current Diameter
+        int currD = left + right;
+        D = max(D, currD);
+        return 1 + max(left, right);
     }
-
-    int diameterOfBinaryTree(TreeNode* root) {
-        // base case
-        if(root == NULL){
-            return 0;
-        }
-        int option1 = diameterOfBinaryTree(root->left);
-        int option2 = diameterOfBinaryTree(root->right);
-        int option3 = height(root->left) + height(root->right);
-        int diameter = max(option1, max(option2, option3));
-
-        return diameter;
+    int diameterOfBinaryTree(TreeNode* root){
+        height(root);
+        return D;
     }
 };
