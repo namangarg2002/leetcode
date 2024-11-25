98678818de4f27649df922690ad879a162c7b5a9
Naman <98084335+namangarg2002@users.noreply.github.com>
2024-11-25 11:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (31.10%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,37 +11,56 @@
  */
 class Solution {
 public:
+    // int height(TreeNode* root){
+    //     if(root == NULL){
+    //        return true; 
+    //     }
+    //     int leftHeight = height(root->left);
+    //     int rightHeight = height(root->right);
+    //     int finalAns = max(leftHeight, rightHeight) + 1;
+    //     return finalAns;
+    // }
+    // bool isBalanced(TreeNode* root) {
+    //     // base case
+    //     if(root == NULL){
+    //         return true;
+    //     }
+
+    //     // currNode = solve
+    //     int leftHeight = height(root->left);
+    //     int rightHeight = height(root->right);
+
+    //     int diff = abs(leftHeight-rightHeight);
+
+    //     bool currNodeAns = (diff <= 1);
+
+    //     int leftAns = isBalanced(root->left);
+    //     int rightAns = isBalanced(root->right);
+
+    //     if(currNodeAns && leftAns && rightAns){
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
+    bool isbalanced = true;
     int height(TreeNode* root){
-        if(root == NULL){
-           return true; 
+        if(!root) return 0;
+
+        int left = height(root->left);
+        int right = height(root->right);
+
+        //check for current Node, is balanced or not
+        if(isbalanced && abs(left - right)>1){
+            isbalanced = false;
         }
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        int finalAns = max(leftHeight, rightHeight) + 1;
-        return finalAns;
+
+        return 1 + max(left, right);
     }
-    bool isBalanced(TreeNode* root) {
-        // base case
-        if(root == NULL){
-            return true;
-        }
-
-        // currNode = solve
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-
-        int diff = abs(leftHeight-rightHeight);
-
-        bool currNodeAns = (diff <= 1);
-
-        int leftAns = isBalanced(root->left);
-        int rightAns = isBalanced(root->right);
-
-        if(currNodeAns && leftAns && rightAns){
-            return true;
-        }
-        else{
-            return false;
-        }
+    bool isBalanced(TreeNode* root){
+        height(root);
+        return isbalanced;
     }
+
 };
