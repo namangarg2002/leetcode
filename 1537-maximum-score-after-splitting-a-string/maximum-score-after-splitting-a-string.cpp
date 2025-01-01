07a95fb0368105d23e1c1fa904e6c6979ad2334e
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-01 22:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (52.60%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public: 
     int maxScore(string s) {
-        // M1: Brute Force (1st jan 2025)
+        // // M1: Brute Force (1st jan 2025)
         // int n = s.length();
         // int result = INT_MIN;
 
@@ -24,26 +24,44 @@ public:
         // }
         // return result;
 
+        // // M2: 2 pass Approach
+        // int n = s.length();
+        // int result = INT_MIN;
+
+        // int total_ones = count(begin(s), end(s), '1'); //1st pass
+
+        // int zeros = 0;
+        // int ones = 0;
+        // // right_ones = total_ones - ones
+
+        // for(int i=0; i<=n-2; i++){
+        //     if(s[i] == '0'){
+        //         zeros++;
+        //     }
+        //     else{
+        //         ones++;
+        //     }
+        //     int right_ones = total_ones - ones;
+        //     result = max(result, zeros+right_ones);
+        // }
+        // return result;
+
+        // M3: One pass Algo
         int n = s.length();
-        int result = INT_MIN;
 
-        int total_ones = count(begin(s), end(s), '1'); //1st pass
-
+        int score = INT_MIN;
         int zeros = 0;
         int ones = 0;
-        // right_ones = total_ones - ones
-
         for(int i=0; i<=n-2; i++){
-            if(s[i] == '0'){
-                zeros++;
-            }
-            else{
-                ones++;
-            }
-            int right_ones = total_ones - ones;
-            result = max(result, zeros+right_ones);
+            if(s[i]=='1') ones++;
+            else zeros++;
+            score = max(score, zeros-ones);
         }
-        return result;
+        if(s[n-1]=='1'){
+            ones++;
+        }
+
+        return score + ones;
 
 
     }
