cae422d90564fb553a99ce096999444f0b9e0fda
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-01 17:41:32 +0530

    Time: 23 ms (5.62%) | Memory: 28.5 MB (5.88%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,29 +1,82 @@
+class TrieNode
+{
+public:
+  char value;
+  TrieNode *children[26];
+  bool isTerminal;
+  int childCount;
+
+  TrieNode(char val)
+  {
+    this->value = val;
+    for (int i = 0; i < 26; i++)
+    {
+      children[i] = NULL;
+    }
+    this->isTerminal = false;
+    this->childCount = 0;
+  }
+};
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        int i=0;
-        while(true){
-            char curr_ch = 0;
-            for(auto str : strs){
-                if(i >= str.size()){
-                    // out of Bound
-                    curr_ch = 0;
-                    break;
-                }
-                if(curr_ch == 0){
-                    curr_ch = str[i];
-                }else if(str[i] != curr_ch){
-                    curr_ch = 0;
-                    break;
-                }
-            }
-            if(curr_ch == 0){
-                break;
-            }
-            ans.push_back(curr_ch);
-            i++;
+    void insertWord(TrieNode *root, string word, int index = 0)
+    {
+        if (index == word.length())
+        {
+            root->isTerminal = true;
+            return;
         }
+        char ch = word[index];
+        int childindex = ch - 'a';
+        TrieNode *child;
+        if (root->children[childindex] != NULL)
+        {
+            // child present
+            child = root->children[childindex];
+        }
+        else
+        {
+            // absent
+            child = new TrieNode(ch);
+            root->children[childindex] = child;
+            root->childCount++;
+        }
+        // recurssion will handle further
+        insertWord(child, word, index + 1);
+    }
+
+    void findLCP(string &ans, TrieNode* root){
+        // base case
+        if(root->isTerminal == true){
+            return;
+        }
+
+        TrieNode* child;
+        // abhi root node par hu
+        if(root->childCount == 1){
+            // child tak jaao
+            for(int i=0; i<26; i++){
+                if(root->children[i] != NULL){
+                    child = root->children[i];
+                }
+            }
+            // ans mai store karwao
+            ans.push_back(child->value);
+        }else{
+            return;
+        }
+        // rest recurssion will handle it
+        findLCP(ans, child);
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root = new TrieNode('-');
+        // insert all strings
+        for(auto str: strs){
+            insertWord(root, str);
+        }
+        string ans = "";
+        // string str = strs[0];
+        findLCP(ans, root);
         return ans;
     }
 };
