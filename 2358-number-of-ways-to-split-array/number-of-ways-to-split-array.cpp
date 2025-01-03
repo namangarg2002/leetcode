a228e2222228628d36c6aa6cce3fa03719584d8c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-03 21:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 89.5 MB (44.20%) - LeetSync

---
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        long long totalSum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long leftSum = 0; 
+        int count = 0; 
+
+        for (int i = 0; i < n - 1; ++i) {
+            leftSum += nums[i];
+            long long rightSum = totalSum - leftSum;
+            
+            if (leftSum >= rightSum) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
