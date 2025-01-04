4b7dd68779056f5e73a7e770f49cd1cfecaf7f4c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-04 16:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (59.73%) - LeetSync

--- 1080-camelcase-matching/camelcase-matching.cpp
+++ 1080-camelcase-matching/camelcase-matching.cpp
@@ -1,43 +1,3 @@
-class Trie {
-private:
-    struct TrieNode {
-        unordered_map<char, TrieNode*> children;
-        bool isEnd = false;
-    };
-
-    TrieNode* root;
-
-public:
-    Trie() {
-        root = new TrieNode();
-    }
-    
-    void insert(const string& word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            if (!node->children[c]) {
-                node->children[c] = new TrieNode();
-            }
-            node = node->children[c];
-        }
-        node->isEnd = true;
-    }
-    
-    bool match(const string& query) {
-        TrieNode* node = root;
-        for( char c:query){
-            if(node->children.count(c)){
-                node = node->children[c];
-                // if we reach the end of a valid pattern
-                if(node->isEnd) return true;
-            }else if(isupper(c)){
-                // if the character is upper and not in the Trie, its invalid
-                return false;
-            }
-        }
-        return node->isEnd;
-    }
-};
 class Solution {
     bool isMatch(const string& query, const string& pattern) {
         int i = 0;
@@ -53,8 +13,6 @@ class Solution {
 public:
     vector<bool> camelMatch(vector<string>& queries, string pattern) {
         vector<bool> result;
-        Trie trie;
-        trie.insert(pattern);
         for(const string&query:queries){
             result.push_back(isMatch(query, pattern));
         }        
