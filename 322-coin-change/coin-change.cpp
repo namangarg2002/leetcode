2a9da623e8b17d32129e0638134449ab6a4b1841
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-06 13:55:55 +0530

    Time: 12 ms (96.26%) | Memory: 17.9 MB (69.06%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -54,14 +54,47 @@ public:
         return dp[amount];
         
     } 
+
+    // Tabulation Approach
+    int solveUsingTabulation(vector<int>& coins, int amount){
+        // step 2:
+        int n = amount;
+        vector<int>dp(n+1, INT_MAX);
+
+        // Step2: base case analyse
+        dp[0] = 0;
+
+        // Step3: for loop
+        for(int value=1; value<=amount; value++){
+            int mini = INT_MAX;
+            for(int i=0; i<coins.size(); i++){
+                if(value - coins[i] >= 0){
+                    int recurssionKaAns = dp[value-coins[i]];
+                    // if a valid ans
+                    if(recurssionKaAns != INT_MAX){
+                        // considering usage of ith coin
+                        int ans = 1 + recurssionKaAns;
+                        mini = min(mini, ans);
+                    }
+                }
+            }
+            dp[value] = mini;
+        }
+
+        return dp[amount];
+
+    }
     int coinChange(vector<int>& coins, int amount) {
         // // Recursive Approach
         // int ans = solveUsingRecurssion(coins, amount);
 
         // Memoization Approach
-        int n = amount;
-        vector<int>dp(n+1, -1);
-        int ans = solveUsingMemoization(coins, amount, dp);
+        // int n = amount;
+        // vector<int>dp(n+1, -1);
+        // int ans = solveUsingMemoization(coins, amount, dp);
+
+        // Tabulation Approach
+        int ans = solveUsingTabulation(coins, amount);
         return (ans == INT_MAX) ? -1 : ans;
     }
 };
