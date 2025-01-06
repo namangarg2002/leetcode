f0d5e67b5990d35909fbe1d9583dce28598da8ff
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-06 11:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (15.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // Recurssive Approach 
+    int solveUsingRecurssion(vector<int>& nums, int index){
+        // base case
+        if(index >= nums.size()){
+            return 0;
+        }
+
+        // recurssion relation
+        int include = nums[index] + solveUsingRecurssion(nums, index+2);
+        int exclude = 0 + solveUsingRecurssion(nums, index+1);
+
+        return max(include, exclude);
+    }
+
+    // Memoization Approach 
     int solveUsingMemoization(vector<int>&nums, int index, vector<int>&dp){
         // base case
         if(index >= nums.size()){
@@ -19,19 +34,25 @@ public:
         return dp[index];
     }
 
+    // Tabulation approach
+    int solveUsingTabulation(vector<int>& nums){
+        int n = nums.size();
+        vector<int>dp(n, -1);
 
-    int solveUsingRecurssion(vector<int>& nums, int index){
-        // base case
-        if(index >= nums.size()){
-            return 0;
+        dp[n-1] = nums[n-1];
+
+        for(int index=n-2; index>=0; index--){
+            int tempAns = 0;
+            if(index + 2 < n){
+                tempAns = dp[index+2];
+            }
+            int include = nums[index] + tempAns;
+            int exclude = 0 + dp[index+1];
+            dp[index] = max(include, exclude);
         }
-
-        // recurssion relation
-        int include = nums[index] + solveUsingRecurssion(nums, index+2);
-        int exclude = 0 + solveUsingRecurssion(nums, index+1);
-
-        return max(include, exclude);
+        return dp[0];
     }
+
     int rob(vector<int>& nums) {
         // from L->R direction
 
@@ -39,11 +60,14 @@ public:
         // int index = 0;
         // int ans = solveUsingRecurssion(nums, index);
 
-        // solve using Memoization
-        int index = 0;
-        int n = nums.size();
-        vector<int>dp(n+1, -1);
-        int ans = solveUsingMemoization(nums, index, dp);
+        // // solve using Memoization
+        // int index = 0;
+        // int n = nums.size();
+        // vector<int>dp(n+1, -1);
+        // int ans = solveUsingMemoization(nums, index, dp);
+
+        // solve using tabulation
+        int ans = solveUsingTabulation(nums);
 
         return ans;
     }
