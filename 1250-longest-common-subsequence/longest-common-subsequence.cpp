7f4062f28b2e94e2efdc50735e82d94f79a60d96
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-08 13:25:03 +0530

    Time: 23 ms (78.76%) | Memory: 27.4 MB (47.89%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -42,15 +42,39 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTabulation(string &a, string &b, int i, int j){
+        vector<vector<int>>dp(a.length()+1, vector<int>(b.length()+1, 0));
+
+        for(int i_index=a.length()-1; i_index>=0; i_index--){
+            for(int j_index=b.length()-1; j_index>=0; j_index--){
+                int ans = 0;
+                if(a[i_index] == b[j_index]){
+                    ans = 1 + dp[i_index+1][j_index+1];
+                }else{
+                    ans = max(dp[i_index+1][j_index], dp[i_index][j_index+1]);
+                }
+
+                dp[i_index][j_index] = ans;
+            }
+        }
+
+        return dp[0][0];
+
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         // // recursive approach
         // int i=0;
         // int j=0;
         // int ans = solveUsingRecursion(text1, text2, i, j);
 
-        // Memoization Approach
-        vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, -1));
-        int ans = solveUsingMemoization(text1, text2, 0, 0, dp);
+        // // Memoization Approach
+        // vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, -1));
+        // int ans = solveUsingMemoization(text1, text2, 0, 0, dp);
+
+        // Tabulation Approach
+
+        int ans = solveUsingTabulation(text1, text2, 0, 0);
 
         return ans;
     }
