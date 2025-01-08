9d0b7ad371508a9abf1af94d0f33b639b6ecd55f
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-08 16:50:06 +0530

    Time: 7 ms (57.03%) | Memory: 13.1 MB (48.83%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -52,13 +52,47 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTabulation(string &a, string &b){
+        vector<vector<int>>dp(a.length()+1, vector<int>(b.length()+1, -1));
+
+        // base case analyse
+        for(int col=0; col<=b.length(); col++){
+            dp[a.length()][col] = b.length()-col;
+        }
+        for(int row=0; row<=a.length(); row++){
+            dp[row][b.length()] = a.length()-row;
+        }
+
+        for(int i=a.length()-1; i>=0; i--){
+            for(int j=b.length()-1; j>=0; j--){
+                int ans = 0;
+                if(a[i] == b[j]){
+                    ans = 0 + dp[i+1][j+1];
+                }else{
+                    int replace = 1 + dp[i+1][j+1];
+                    int insert = 1 + dp[i][j+1];
+                    int remove = 1 + dp[i+1][j];
+
+                    ans = min(replace, min(insert, remove));
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+
+    }
+
     int minDistance(string word1, string word2) {
         // // recursive Aprroach
         // int ans = solveUsingRecursion(word1, word2, 0, 0);
 
-        // Memoization approach
-        vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, -1));
-        int ans = solveUsingMemoization(word1, word2, 0, 0, dp);
+        // // Memoization approach
+        // vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, -1));
+        // int ans = solveUsingMemoization(word1, word2, 0, 0, dp);
+
+        // Tabulation Approach
+        int ans = solveUsingTabulation(word1, word2);
 
         return ans;
     }
