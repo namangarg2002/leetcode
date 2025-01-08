d280550f4d4a623d8637d308dcad36317a390f16
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-08 17:10:36 +0530

    Time: 7 ms (57.03%) | Memory: 13 MB (62.68%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -53,6 +53,38 @@ public:
     }
 
     int solveUsingTabulation(string &a, string &b){
+        vector<int>curr(a.length()+1, 0);
+        vector<int>next(a.length()+1, 0);
+
+        // // base case analyse
+        // for(int col=0; col<=b.length(); col++){
+        //     curr[col] = b.length()-col;
+        // }
+        for(int row=0; row<=a.length(); row++){
+            next[row] = a.length()-row;
+        }
+        for(int j_index=b.length()-1; j_index>=0; j_index--){
+            for(int i_index=a.length()-1; i_index>=0; i_index--){
+                int ans = 0;
+                if(a[i_index] == b[j_index]){
+                    ans = 0 + next[i_index+1];
+                }else{
+                    int replace = 1 + next[i_index+1];
+                    int insert = 1 + next[i_index];
+                    int remove = 1 + curr[i_index+1];
+
+                    ans = min(replace, min(insert, remove));
+                }
+
+                curr[i_index] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+
+    }
+
+    int solveUsingTabulationSpaceOptimization(string &a, string &b){
         vector<vector<int>>dp(a.length()+1, vector<int>(b.length()+1, -1));
 
         // base case analyse
@@ -63,20 +95,20 @@ public:
             dp[row][b.length()] = a.length()-row;
         }
 
-        for(int i=a.length()-1; i>=0; i--){
-            for(int j=b.length()-1; j>=0; j--){
+        for(int i_index=a.length()-1; i_index>=0; i_index--){
+            for(int j_index=b.length()-1; j_index>=0; j_index--){
                 int ans = 0;
-                if(a[i] == b[j]){
-                    ans = 0 + dp[i+1][j+1];
+                if(a[i_index] == b[j_index]){
+                    ans = 0 + dp[i_index+1][j_index+1];
                 }else{
-                    int replace = 1 + dp[i+1][j+1];
-                    int insert = 1 + dp[i][j+1];
-                    int remove = 1 + dp[i+1][j];
+                    int replace = 1 + dp[i_index+1][j_index+1];
+                    int insert = 1 + dp[i_index][j_index+1];
+                    int remove = 1 + dp[i_index+1][j_index];
 
                     ans = min(replace, min(insert, remove));
                 }
 
-                dp[i][j] = ans;
+                dp[i_index][j_index] = ans;
             }
         }
         return dp[0][0];
@@ -91,8 +123,11 @@ public:
         // vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, -1));
         // int ans = solveUsingMemoization(word1, word2, 0, 0, dp);
 
-        // Tabulation Approach
-        int ans = solveUsingTabulation(word1, word2);
+        // // Tabulation Approach
+        // int ans = solveUsingTabulation(word1, word2);
+
+        // Space Optimization
+        int ans = solveUsingTabulationSpaceOptimization(word1, word2);
 
         return ans;
     }
