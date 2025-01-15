992529e6cb9988f2ce70d764eb679c3fe5adc5a4
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-15 12:47:37 +0530

    Time: 565 ms (14.09%) | Memory: 296.7 MB (5.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
-    int topdown(vector<int>& nums, int curr, int prev, vector<vector<int>>& dp){
+    int solveUsingRecursion(vector<int>& nums, int curr, int prev){
+        // base case
         if(curr >= nums.size()){
             return 0;
         }
+
+        int include = 0;
+        if(prev == -1 || nums[curr] > nums[prev]){
+            include = 1 + solveUsingRecursion(nums, curr+1, curr);
+        }
+
+        int exclude = 0 + solveUsingRecursion(nums, curr+1, prev);
+        int ans = max(include, exclude);
+        return ans;
+
+    }
+
+    int solveUsingMemoization(vector<int>& nums, int curr, int prev, vector<vector<int>>&dp){
+        if(curr >= nums.size()){
+            return 0;
+        }
+
         if(dp[curr][prev+1] != -1){
             return dp[curr][prev+1];
         }
+
         int include = 0;
         if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1 + topdown(nums, curr+1, curr, dp);
+            include = 1 + solveUsingMemoization(nums, curr+1, curr, dp);
         }
-        int exclude = topdown(nums, curr+1, prev, dp);
+        int exclude = 0 + solveUsingMemoization(nums, curr+1, prev, dp);
 
-        return dp[curr][prev+1] = max(include, exclude);
+        dp[curr][prev+1] = max(include, exclude);
+
+        return dp[curr][prev+1];
     }
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
         int prev = -1;
-        vector<vector<int>> dp(nums.size()+1, vector<int>(nums.size(), -1));
-        int ans = topdown(nums, curr, prev, dp);
+
+        // // Recusive Approach
+        // int ans = solveUsingRecursion(nums, curr, prev);
+
+        // Memoization Approach
+        int n = nums.size(); 
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        int ans = solveUsingMemoization(nums, curr, prev, dp);
+
         return ans;
     }
 };
