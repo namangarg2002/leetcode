1f63091df81953817dd1cd0fba08b02e8f7d416e
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-17 19:41:52 +0530

    Time: 24 ms (7.80%) | Memory: 34.8 MB (23.40%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        int n = s.size();
-        if (n % 2 != 0) return false; 
-        int openUnlocked = 0, closeUnlocked = 0; 
-        int balance = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (locked[i] == '1') {
-                balance += (s[i] == '(' ? 1 : -1);
-            } else {
-                openUnlocked++;
-            }
-            if (balance + openUnlocked < 0) return false;
+        int n = s.length();
+        if(n & 1){
+            // odd
+            return false;
+        }
+        if(s.front() == ')' && locked.front()==1){
+            return false;
+        }
+        if(s.back() == '(' && locked.back()==1){
+            return false;
         }
 
-        balance = 0;
-        closeUnlocked = 0;
+        stack<int>open;
+        stack<int>openClose;
 
-        for (int i = n - 1; i >= 0; --i) {
-            if (locked[i] == '1') {
-                balance += (s[i] == ')' ? 1 : -1);
-            } else {
-                closeUnlocked++;
+        for(int i=0; i<n; i++){
+            if(locked[i] == '0'){
+                openClose.push(i);
+            }else{
+                if(s[i] == '('){
+                    open.push(i);
+                }
+                if(s[i] == ')'){
+                    if(!open.empty()){
+                        open.pop();
+                    }else if(!openClose.empty()){
+                        openClose.pop();
+                    }else{
+                        return false;
+                    }
+                }
+
             }
-            if (balance + closeUnlocked < 0) return false; 
+        }
+        while(!open.empty() && !openClose.empty() && open.top()< openClose.top()){
+            open.pop();
+            openClose.pop();
         }
 
-        return true;
+        return (open.empty());
+
     }
 };
