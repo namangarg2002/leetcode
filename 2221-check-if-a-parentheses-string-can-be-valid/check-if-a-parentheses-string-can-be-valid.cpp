7de844886ad0aa45cea8ae440133d518479076c3
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-17 20:44:35 +0530

    Time: 15 ms (44.80%) | Memory: 30.2 MB (57.76%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,46 +1,76 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
+        // M1: Using Stack
+        // int n = s.length();
+        // if(n & 1){
+        //     // odd
+        //     return false;
+        // }
+        // if(s.front() == ')' && locked.front()==1){
+        //     return false;
+        // }
+        // if(s.back() == '(' && locked.back()==1){
+        //     return false;
+        // }
+
+        // stack<int>open;
+        // stack<int>openClose;
+
+        // for(int i=0; i<n; i++){
+        //     if(locked[i] == '0'){
+        //         openClose.push(i);
+        //     }else{
+        //         if(s[i] == '('){
+        //             open.push(i);
+        //         }
+        //         if(s[i] == ')'){
+        //             if(!open.empty()){
+        //                 open.pop();
+        //             }else if(!openClose.empty()){
+        //                 openClose.pop();
+        //             }else{
+        //                 return false;
+        //             }
+        //         }
+
+        //     }
+        // }
+        // while(!open.empty() && !openClose.empty() && open.top()< openClose.top()){
+        //     open.pop();
+        //     openClose.pop();
+        // }
+
+        // return (open.empty()) ? true : false;
+
+        // M2: LtoR iteration and RtoL iteration
         int n = s.length();
-        if(n & 1){
-            // odd
-            return false;
-        }
-        if(s.front() == ')' && locked.front()==1){
-            return false;
-        }
-        if(s.back() == '(' && locked.back()==1){
-            return false;
-        }
 
-        stack<int>open;
-        stack<int>openClose;
+        if(n&1) return false;
 
+        // L to R
+        int open = 0;
         for(int i=0; i<n; i++){
-            if(locked[i] == '0'){
-                openClose.push(i);
+            if(s[i]=='(' || locked[i]=='0'){
+                open++;
             }else{
-                if(s[i] == '('){
-                    open.push(i);
-                }
-                if(s[i] == ')'){
-                    if(!open.empty()){
-                        open.pop();
-                    }else if(!openClose.empty()){
-                        openClose.pop();
-                    }else{
-                        return false;
-                    }
-                }
-
+                open--;
             }
+            if(open < 0) return false;
         }
-        while(!open.empty() && !openClose.empty() && open.top()< openClose.top()){
-            open.pop();
-            openClose.pop();
+        
+        // R to L
+        int close = 0;
+        for(int i=n-1; i>=0; i--){
+            if(s[i]==')' || locked[i]=='0'){
+                close++;
+            }else{
+                close--;
+            }
+            if(close < 0) return false;
         }
 
-        return (open.empty()) ? true : false;
+        return true;
 
     }
 };
