ca7d476589c30c447b987bedea3cebfa96fc7b07
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-22 21:13:37 +0530

    Time: 15 ms (77.52%) | Memory: 18.2 MB (68.30%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(string &s1, string &s2, int i, int j, vector<vector<int>>&dp){
+    int solveMem(string &s1, string &s2, int i, int j, vector<vector<int>>&dp){
         int cost = 0;
         if(i==s1.size() || j==s2.size()){
             // return the sum of Ascii of remaining char of remaining string of s1 string 
@@ -17,18 +17,45 @@ public:
             return dp[i][j];
         }
         else if(s1[i] == s2[j]){
-            cost = solve(s1, s2, i+1, j+1, dp);
+            cost = solveMem(s1, s2, i+1, j+1, dp);
         }else{
-            int cost1 = s1[i] + solve(s1, s2, i+1, j, dp);
-            int cost2 = s2[j] + solve(s1, s2, i, j+1, dp);
+            int cost1 = s1[i] + solveMem(s1, s2, i+1, j, dp);
+            int cost2 = s2[j] + solveMem(s1, s2, i, j+1, dp);
 
             cost = min(cost1, cost2);
         }
         dp[i][j] = cost;
         return dp[i][j];
     }
+    int solveTab(string &s1, string &s2){
+        vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, 0));
+        // initialize base case when one of the the string is empty
+        for(int i=s1.size()-1; i>=0; i--){
+            dp[i][s2.size()] = s1[i] + dp[i+1][s2.size()];
+        }
+        for(int i=s2.size()-1; i>=0; i--){
+            dp[s1.size()][i] = s2[i] + dp[s1.size()][i+1];
+        }
+
+        int cost = 0;
+        for(int i=s1.size()-1; i>=0; i--){
+            for(int j=s2.size()-1; j>=0; j--){
+                if(s1[i] == s2[j]){
+                    cost = dp[i+1][j+1];
+                }else{
+                    int cost1 = s1[i] + dp[i+1][j];
+                    int cost2 = s2[j] + dp[i][j+1];
+
+                    cost = min(cost1, cost2);
+                }
+                dp[i][j] = cost;
+            }
+        }
+        return dp[0][0];
+    }
     int minimumDeleteSum(string s1, string s2) {
-        vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return solve(s1, s2, 0, 0, dp);
+        // vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+        // return solveMem(s1, s2, 0, 0, dp);
+        return solveTab(s1, s2);
     }
 };
