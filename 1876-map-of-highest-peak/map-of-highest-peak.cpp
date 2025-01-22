f04c8b45cd203bf14aabe5716061a343ffa3f2d0
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-22 22:02:10 +0530

    Time: 65 ms (89.68%) | Memory: 112.9 MB (59.10%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int rows = isWater.size();
+        int cols = isWater[0].size();
+        
+        vector<vector<int>> height(rows, vector<int>(cols, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        // Perform BFS
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto [dx, dy] : directions) {
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && height[nx][ny] == -1) {
+                    height[nx][ny] = height[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        return height;
+    }
+};
