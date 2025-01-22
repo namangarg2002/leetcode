d6d3b0c67683cd561ce8428a87ae03b5e3b35da6
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-22 17:21:41 +0530

    Time: 79 ms (37.75%) | Memory: 13.9 MB (35.07%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -22,6 +22,28 @@ public:
         dp[n] = ans;
         return dp[n];
     }
+
+    int numSquareHelper(int n){
+        vector<int>dp(n+1, 0);
+
+        dp[0] = 1;
+
+        for(int i=1; i<=n; i++){
+            int ans = INT_MAX;
+            int start = 1;
+            int end = sqrt(n);
+            while(start<=end){
+                int perfectSquare = start * start;
+                int numberofPerfectSquare = 1 + numSquareHelper(n-perfectSquare, dp);
+                if(numberofPerfectSquare < ans){
+                    ans = numberofPerfectSquare;
+                }
+                i++;
+            }
+        }
+
+        return dp[n];
+    }
     int numSquares(int n) {
         vector<int>dp(n+1, -1);
         return numSquareHelper(n, dp)-1;
