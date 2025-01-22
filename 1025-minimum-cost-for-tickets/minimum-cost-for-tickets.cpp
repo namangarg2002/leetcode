f434bb97a1d4fa02152d765d63f255985bf892ed
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-22 18:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (55.46%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -36,8 +36,43 @@ public:
         return dp[i];
         
     }
+
+
+    int mincostTicketsHelper(vector<int>& days, vector<int>& costs){
+        vector<int>dp(days.size()+1, 0);
+
+        dp[days.size()] = 0;
+
+        int ans = INT_MAX;
+        for(int index=days.size()-1; index>=0; index--){
+            // 1 day pass taken
+            int cost1 = costs[0] + dp[index+1];
+
+            // 7 Day Pass taken
+            int passEndDay = days[index] + 7 - 1;
+            int j = index;
+            while(j<days.size() && days[j] <= passEndDay ){
+                j++;
+            }
+            int cost7 = costs[1] + dp[j];
+
+            // 30 Day Pass taken
+            passEndDay = days[index] + 30 - 1;
+            j = index;
+            while(j<days.size() && days[j] <= passEndDay ){
+                j++;
+            }
+            int cost30 = costs[2] + dp[j];
+
+            ans = min(cost1, min(cost7, cost30));
+            dp[index] = ans;
+        }
+
+        return dp[0];
+        
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1, -1);
-        return mincostTicketsHelper(days, costs, 0, dp);
+        // vector<int>dp(days.size()+1, -1);
+        return mincostTicketsHelper(days, costs);
     }
 };
