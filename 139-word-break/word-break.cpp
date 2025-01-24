fdeecdd34a2c6fd656fc011ac9b8cbe0501cd055
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-24 17:09:15 +0530

    Time: 3 ms (72.35%) | Memory: 10.7 MB (79.44%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -27,12 +27,36 @@ public:
 
         return dp[start];
     }
+    bool solveTab(string &s, vector<string>& wordDict){
+        vector<int>dp(s.size()+1, 0);
+
+        dp[s.size()] = true;
+
+        for(int start=s.size()-1; start>=0; start--){
+            // one case solve
+            string word = "";
+            for(int i=start; i<s.size(); i++){
+                word += s[i];
+                if(check(wordDict, word)){
+                    if(dp[i+1]){
+                        dp[start]  = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
         // // Recursive Approach
         // return solveRE(s, wordDict, 0);
 
-        // Memoiation Approach
-        vector<int>dp(s.size()+1, -1);
-        return solveMem(s, wordDict, 0, dp);
+        // // Memoiation Approach
+        // vector<int>dp(s.size()+1, -1);
+        // return solveMem(s, wordDict, 0, dp);
+
+        // tabulation Approach
+        return solveTab(s, wordDict);
     }
 };
