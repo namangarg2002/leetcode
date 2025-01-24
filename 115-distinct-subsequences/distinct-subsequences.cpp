bc274a7ea101b6716e120c898dc4d420d0a1ca87
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-24 16:05:44 +0530

    Time: 27 ms (71.94%) | Memory: 26.8 MB (69.74%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -22,12 +22,36 @@ public:
         
         return dp[i][j];
     }
+    long long solveTab(string &s, string &t) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+
+        for (int i = 0; i <= s.size(); i++) {  
+            dp[i][t.size()] = 1;
+        }
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = t.size() - 1; j >= 0; j--) {
+                long long ans = 0;
+                if (s[i] == t[j]) {
+                    ans += dp[i + 1][j + 1];
+                }
+                ans += dp[i + 1][j];
+
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     int numDistinct(string s, string t) {
         // // Recursive Approach
         // return solveRE(s, t, 0, 0); 
 
-        // Memoiation Apprioach
-        vector<vector<int>>dp(s.size()+1, vector<int>(t.size()+1, -1));
-        return solveMem(s, t, 0, 0, dp);
+        // // Memoiation Apprioach
+        // vector<vector<int>>dp(s.size()+1, vector<int>(t.size()+1, -1));
+        // return solveMem(s, t, 0, 0, dp);
+
+        // Tabulation Approach
+        return solveTab(s, t);
     }
 };
