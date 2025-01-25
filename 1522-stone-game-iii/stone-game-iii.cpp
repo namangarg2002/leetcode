5b60ae9d78a3ed9c6da25ea3ae00846c9142b37c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-25 17:09:48 +0530

    Time: 14 ms (81.22%) | Memory: 136.2 MB (64.91%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -30,13 +30,32 @@ public:
         dp[index] = ans;
         return dp[index];
     }
+    int solveTab(vector<int>& stoneValue){
+        vector<int>dp(stoneValue.size()+1, 0);
+
+        for(int index=stoneValue.size()-1; index>=0; index--){
+            int ans = INT_MIN;
+            int total = 0;
+            for(int X=1; X<=3; X++){
+                if(index + X - 1 >= stoneValue.size()) break;
+                total += stoneValue[index + X - 1];
+
+                ans = max(ans, total - dp[index+X]);
+            }
+            dp[index] = ans;
+        }
+        return dp[0];
+    }
     string stoneGameIII(vector<int>& stoneValue) {
         // // Recursion Approach
         // int ans =  solveRE(stoneValue, 0); // A-B
 
-        // Memoiation Approach
-        vector<int>dp(stoneValue.size()+1, -1);
-        int ans = solveMem(stoneValue, 0, dp);
+        // // Memoiation Approach
+        // vector<int>dp(stoneValue.size()+1, -1);
+        // int ans = solveMem(stoneValue, 0, dp);
+
+        // Tabulation Approach
+        int ans = solveTab(stoneValue);
 
         if(ans > 0) return "Alice";
         if(ans < 0) return "Bob";
