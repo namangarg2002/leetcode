9557f6bd7e46d2c52e99b244ac8c13f7fe29ce8c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-25 14:40:14 +0530

    Time: 139 ms (9.60%) | Memory: 31 MB (20.07%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -45,13 +45,41 @@ public:
 
         return dp[index][M][alice];
     }
+    int solveTab(vector<int>& piles){
+        vector<vector<vector<int>>>dp(piles.size()+1, vector<vector<int>>(piles.size()+1, vector<int>(2, 0)));
+
+        for(int index = piles.size()-1; index >=0; index--){
+            for(int M=piles.size(); M>=1; M--){
+                for(int alice=0; alice<=1; alice++){
+
+                    int ans = alice ? INT_MIN : INT_MAX;
+                    int total = 0;
+
+                    for(int X=1; X<=2*M; X++){
+                        if(index+X-1 >= piles.size()) break;
+                        total += piles[index+X-1];
+                        if(alice){
+                            ans = max(ans, total + dp[index + X][max(X, M)][!alice]);
+                        }else{
+                            ans = min(ans, dp[index + X][max(X, M)][!alice]);
+                        }
+                    }
+                    dp[index][M][alice] =  ans;
+                }
+            }
+        }
+        return dp[0][1][true];
+    }
     int stoneGameII(vector<int>& piles) {
 
         // // recursion Approach
         // return solveRE(piles, 0, 1, true);
 
-        // Memoiation Approach
-        vector<vector<vector<int>>>dp(piles.size()+1, vector<vector<int>>(piles.size()+1, vector<int>(2, -1)));
-        return solveMem(piles, 0, 1, true, dp);
+        // // Memoiation Approach
+        // vector<vector<vector<int>>>dp(piles.size()+1, vector<vector<int>>(piles.size()+1, vector<int>(2, -1)));
+        // return solveMem(piles, 0, 1, true, dp);
+
+        // Tabulation Approach
+        return solveTab(piles);
     }
 };
