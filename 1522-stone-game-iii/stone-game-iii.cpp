c5df0e8bec3bb809d6c37fddcd01755d09e26c5e
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-25 17:01:23 +0530

    Time: 47 ms (35.58%) | Memory: 140.8 MB (50.91%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int solveRE(vector<int>& stoneValue, int index){
+        if(index >= stoneValue.size()) return 0;
+
+        int ans = INT_MIN;
+        int total = 0;
+        for(int X=1; X<=3; X++){
+            if(index + X - 1 >= stoneValue.size()) break;
+            total += stoneValue[index + X - 1];
+
+            ans = max(ans, total - solveRE(stoneValue, index + X));
+        }
+        return ans;
+    }
+    int solveMem(vector<int>& stoneValue, int index, vector<int>&dp){
+        if(index >= stoneValue.size()) return 0;
+
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int ans = INT_MIN;
+        int total = 0;
+        for(int X=1; X<=3; X++){
+            if(index + X - 1 >= stoneValue.size()) break;
+            total += stoneValue[index + X - 1];
+
+            ans = max(ans, total - solveMem(stoneValue, index + X, dp));
+        }
+        dp[index] = ans;
+        return dp[index];
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        // // Recursion Approach
+        // int ans =  solveRE(stoneValue, 0); // A-B
+
+        // Memoiation Approach
+        vector<int>dp(stoneValue.size()+1, -1);
+        int ans = solveMem(stoneValue, 0, dp);
+
+        if(ans > 0) return "Alice";
+        if(ans < 0) return "Bob";
+        return "Tie";
+    }
+};
