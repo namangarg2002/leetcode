dbfd181741082f1e07a71e82d0e8095a3f2f2d8c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-27 22:58:41 +0530

    Time: 25 ms (9.11%) | Memory: 85.6 MB (92.09%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        vector<int> prefixCommonArray(n, 0);
-        unordered_set<int> seenA, seenB;
-        int commonCount = 0;
 
-        for (int i = 0; i < n; ++i) {
-
-            seenA.insert(A[i]);
-            seenB.insert(B[i]);
-
-            if (A[i] == B[i]) {
-                commonCount++;
-            } else {
-                if (seenB.count(A[i])) {
-                    commonCount++;
-                }
-                if (seenA.count(B[i])) {
-                    commonCount++;
+        vector<int>result(n);
+        for(int i=0; i<n; i++){
+            int count = 0;
+            for(int A_i=0; A_i<=i; A_i++){
+                for(int B_i=0; B_i<=i; B_i++){
+                    if(B[B_i]==A[A_i]){
+                        count++;
+                        break;
+                    }
                 }
             }
-
-            prefixCommonArray[i] = commonCount;
-        }
-
-        return prefixCommonArray;
+            result[i] = count; 
+        } 
+        return result; 
     }
 };
