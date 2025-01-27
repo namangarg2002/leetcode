6d1aba3e3a2e9dd8f41afb0b07465b481a2af5f7
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-27 13:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (74.51%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -29,12 +29,35 @@ public:
         dp[i][j] = flag;
         return dp[i][j];
     }
+    bool solveTab(string &s1, string &s2, string &s3){
+        if (s1.size() + s2.size() != s3.size()) {
+            return false;
+        }
+        vector<vector<bool>>dp(s1.size()+1, vector<bool>(s2.size()+1, false));
+
+        dp[s1.size()][s2.size()] = true;
+        for(int i=s1.size(); i>=0; i--){
+            for(int j=s2.size(); j>=0; j--){
+                int k = i + j;
+                if(i<s1.size() && s1[i] == s3[k]){
+                    dp[i][j] = dp[i][j] || dp[i+1][j];
+                }
+                if(j<s2.size() && s2[j] == s3[k]){
+                    dp[i][j] = dp[i][j] || dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
     bool isInterleave(string s1, string s2, string s3) {
         // // Recursive Aproach
         // return solveRE(s1, s2, s3, 0, 0, 0);
 
-        // Memoiation Approach
-        vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return solveMem(s1, s2, s3, 0, 0 , 0, dp);
+        // // Memoiation Approach
+        // vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+        // return solveMem(s1, s2, s3, 0, 0 , 0, dp);
+
+        // Tabulation Approach
+        return solveTab(s1, s2, s3);
     }
 };
