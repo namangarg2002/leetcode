7b800629332f7edd12bdcfa2bc6579f9b1679e7e
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-28 17:22:58 +0530

    Time: 99 ms (46.29%) | Memory: 95.1 MB (44.77%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -35,12 +35,35 @@ public:
         dp[i][buy] = profit;
         return dp[i][buy];
     }
+    int solveTab(vector<int>& prices, int&fee){
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, 0));
+
+        for(int i=prices.size()-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    int buyItProfit = dp[i+1][0] + (-prices[i]);
+                    int skipProfit = dp[i+1][1];
+                    profit = max(buyItProfit, skipProfit);
+                }else{
+                    int sellItProfit = prices[i] +  dp[i+1][1] - fee;
+                    int skipProfit = dp[i+1][0];
+                    profit = max(sellItProfit, skipProfit);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices, int fee) {
         // // Recursive Approach
         // return solveRE(prices, 0, true, fee);
 
-        // Memoiation Approach
-        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
-        return solveMem(prices, 0, true, fee, dp);
+        // // Memoiation Approach
+        // vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
+        // return solveMem(prices, 0, true, fee, dp);
+
+        // Tabulation Approach
+        return solveTab(prices, fee);
     }
 };
