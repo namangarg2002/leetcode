686b0640725399f5bcc6875cfad5b7bd3906126b
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-28 14:16:39 +0530

    Time: 5 ms (20.43%) | Memory: 19.6 MB (26.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -24,7 +24,7 @@ public:
 
         int profit = 0;
         if(buy){
-            int buyItProfit = solveMem(prices, i+1, 0, dp) + (-prices[i]); //at that day buy
+            int buyItProfit = solveMem(prices, i+1, 0, dp) + (-prices[i]);
             int skipProfit = solveMem(prices, i+1, 1, dp);
             profit = max(buyItProfit, skipProfit);
         }else{
@@ -35,13 +35,36 @@ public:
         dp[i][buy] = profit;
         return dp[i][buy];
     }
+    int solveTab(vector<int>& prices){
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, 0));
+
+        for(int i=prices.size()-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    int buyItProfit = dp[i+1][0] + (-prices[i]);
+                    int skipProfit = dp[i+1][1];
+                    profit = max(buyItProfit, skipProfit);
+                }else{
+                    int sellItProfit = prices[i] +  dp[i+1][1];
+                    int skipProfit = dp[i+1][0];
+                    profit = max(sellItProfit, skipProfit);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
         // // Recursive Approach
         // return solveRE(prices, 0, true);
 
-        // Memoiation Approach
-        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
-        return solveMem(prices, 0, true, dp);
+        // // Memoiation Approach
+        // vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
+        // return solveMem(prices, 0, true, dp);
+
+        // Tabulation Approach
+        return solveTab(prices);
 
     }
 };
