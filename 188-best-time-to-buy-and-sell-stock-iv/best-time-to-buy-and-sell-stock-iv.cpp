7e6fda816af3491098815b4d4f1ee432882843c7
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-28 16:24:06 +0530

    Time: 15 ms (34.47%) | Memory: 18.4 MB (25.17%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int solveRE(vector<int>& prices, int i, int buy, int limit){
+        // base case
+        if(i>=prices.size() || limit == 0) return 0;
+
+        int profit = 0;
+        if(buy){
+            int buyItProfit = solveRE(prices, i+1, 0, limit) + (-prices[i]);
+            int skipProfit = solveRE(prices, i+1, 1, limit);
+            profit = max(buyItProfit, skipProfit);
+        }else{
+            int sellItProfit = prices[i] + solveRE(prices, i+1, 1, limit-1);
+            int skipProfit = solveRE(prices, i+1, 0, limit);
+            profit = max(sellItProfit, skipProfit);
+        }
+        return profit;
+    }
+    int solveMem(vector<int>& prices, int i, int buy, int limit, vector<vector<vector<int>>>&dp){
+        // base case
+        if(i>=prices.size() || limit == 0) return 0;
+
+        if(dp[i][buy][limit] != -1){
+            return dp[i][buy][limit];
+        }
+
+        int profit = 0;
+        if(buy){
+            int buyItProfit = solveMem(prices, i+1, 0, limit, dp) + (-prices[i]);
+            int skipProfit = solveMem(prices, i+1, 1, limit, dp);
+            profit = max(buyItProfit, skipProfit);
+        }else{
+            int sellItProfit = prices[i] + solveMem(prices, i+1, 1, limit-1, dp);
+            int skipProfit = solveMem(prices, i+1, 0, limit, dp);
+            profit = max(sellItProfit, skipProfit);
+        }
+        return dp[i][buy][limit] = profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        // // Recursive Approach
+        // return solveRE(prices, 0, true, k);
+
+        // memoiation Approach
+        vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return solveMem(prices, 0, true, k, dp);
+    }
+};
