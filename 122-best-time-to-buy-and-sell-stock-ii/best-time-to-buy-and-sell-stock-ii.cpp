77098f17ad23ed0a8cdd58825d321e96d29c6670
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-28 14:05:06 +0530

    Time: 7 ms (16.13%) | Memory: 22.1 MB (12.40%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int solveRE(vector<int>&prices, int i, int buy){
+        if(i >= prices.size()) return 0;
+
+        int profit = 0;
+        if(buy){
+            int buyItProfit = solveRE(prices, i+1, 0) + (-prices[i]); //at that day buy
+            int skipProfit = solveRE(prices, i+1, 1);
+            profit = max(buyItProfit, skipProfit);
+        }else{
+            int sellItProfit = prices[i] + solveRE(prices, i+1, 1);
+            int skipProfit = solveRE(prices, i+1, 0);
+            profit = max(sellItProfit, skipProfit);
+        }
+        return profit;
+    }
+    int solveMem(vector<int>&prices, int i, int buy, vector<vector<int>>&dp){
+        if(i >= prices.size()) return 0;
+        
+        if(dp[i][buy] != -1){
+            return dp[i][buy];
+        }
+
+        int profit = 0;
+        if(buy){
+            int buyItProfit = solveMem(prices, i+1, 0, dp) + (-prices[i]); //at that day buy
+            int skipProfit = solveMem(prices, i+1, 1, dp);
+            profit = max(buyItProfit, skipProfit);
+        }else{
+            int sellItProfit = prices[i] + solveMem(prices, i+1, 1, dp);
+            int skipProfit = solveMem(prices, i+1, 0, dp);
+            profit = max(sellItProfit, skipProfit);
+        }
+        dp[i][buy] = profit;
+        return dp[i][buy];
+    }
+    int maxProfit(vector<int>& prices) {
+        // // Recursive Approach
+        // return solveRE(prices, 0, true);
+
+        // Memoiation Approach
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
+        return solveMem(prices, 0, true, dp);
+
+    }
+};
