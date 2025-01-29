52b7fcdea3d23b761f88d3723a3c4c1e8f2d77cf
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-29 23:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (16.67%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -9,8 +9,21 @@ public:
 
         return max(diffByStart, diffByEnd);
     }
+    int solveMem(vector<int>& nums, int start, int end, vector<vector<int>>&dp){
+        if(start==end) return nums[start];
+        
+        if(dp[start][end] != -1) return dp[start][end];
+
+        int diffByStart = nums[start] - solveMem(nums, start+1, end, dp);
+        int diffByEnd = nums[end] - solveMem(nums, start, end-1, dp);
+
+        return dp[start][end] = max(diffByStart, diffByEnd);
+    }
     bool predictTheWinner(vector<int>& nums) {
-        int ans = solveRE(nums, 0, nums.size()-1);
+        // // Recursive Approach
+        // int ans = solveRE(nums, 0, nums.size()-1);
+        vector<vector<int>>dp(nums.size(), vector<int>(nums.size()+1, -1));
+        int ans = solveMem(nums, 0, nums.size()-1, dp);
 
         return (ans >=0) ? true: false;
     }
