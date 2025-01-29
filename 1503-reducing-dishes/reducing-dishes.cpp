d7fc81bb42802f58742b468d307579ed7e0d5ecf
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-29 22:57:35 +0530

    Time: 22 ms (30.45%) | Memory: 24.9 MB (29.93%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -18,6 +18,19 @@ public:
 
         return dp[i][time] = max(include, exclude);
     }
+    int solveTab(vector<int>& sat){
+        vector<vector<int>>dp(sat.size()+1, vector<int>(sat.size()+2, 0));
+
+        for(int i=sat.size()-1; i>=0; i--){
+            for(int time=sat.size(); time>=1; time--){
+                int include = time*sat[i] + dp[i+1][time+1];
+                int exclude = 0 + dp[i+1][time];
+
+                dp[i][time] = max(include, exclude);
+            }
+        }
+        return dp[0][1];
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
 
         sort(satisfaction.begin(), satisfaction.end()); 
@@ -25,8 +38,11 @@ public:
         // // Recursive Approach
         // return solveRE(satisfaction, 0, 1);
 
-        // Memoiation Approach
-        vector<vector<int>>dp(satisfaction.size()+1, vector<int>(satisfaction.size()+1, -1));
-        return solveMem(satisfaction, 0, 1, dp);
+        // // Memoiation Approach
+        // vector<vector<int>>dp(satisfaction.size()+1, vector<int>(satisfaction.size()+1, -1));
+        // return solveMem(satisfaction, 0, 1, dp);
+
+        // Tabulation Approach
+        return solveTab(satisfaction);
     }
 };
