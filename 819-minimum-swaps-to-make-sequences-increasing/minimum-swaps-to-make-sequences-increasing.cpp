bcbf6172d16e1bf82df75de4a06ea8c33d6a4510
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-01-29 22:06:41 +0530

    Time: 90 ms (49.08%) | Memory: 124.7 MB (46.53%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -29,12 +29,38 @@ public:
         dp[i][isSwap] = min(swap, noswap);
         return dp[i][isSwap];
     }
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        vector<vector<int>>dp(nums1.size()+1, vector<int>(2, 0));
+
+        for(int i=nums1.size()-1; i>=1; i--){
+            for(int isSwap=1; isSwap>=0; isSwap--){
+                int prev1 = nums1[i-1], prev2 = nums2[i-1];
+                if(isSwap){
+                    swap(prev1, prev2);
+                }
+                int swap = INT_MAX, noswap = INT_MAX;
+                if(prev1 < nums2[i] && prev2 < nums1[i]){
+                    swap = 1 + dp[i+1][1];
+                }
+                if(prev1 < nums1[i] && prev2 < nums2[i]){
+                    noswap = 0 + dp[i+1][0];
+                }
+                dp[i][isSwap] = min(swap, noswap);
+            }
+        }
+        return dp[1][0];
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         // // recursive Approach
         // return solveRE(nums1, nums2, 0, -1, -1);
 
-        // Memoiation Approach
-        vector<vector<int>>dp(nums1.size(), vector<int>(2, -1));
-        return solveMem(nums1, nums2, 0, -1, -1, dp, 0);
+        // // Memoiation Approach
+        // vector<vector<int>>dp(nums1.size(), vector<int>(2, -1));
+        // return solveMem(nums1, nums2, 0, -1, -1, dp, 0);
+
+        // tabulation Approach
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+        return solveTab(nums1, nums2);
     }
 };
