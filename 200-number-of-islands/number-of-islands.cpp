1a656546ae920fdaa3f7ec5456d81c1110f077e2
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-02-05 16:19:04 +0530

    Time: 95 ms (8.08%) | Memory: 35.3 MB (5.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty()) return 0;
-        int row = grid.size();
-        int col = grid[0].size();
+    bool isSafe(int newX, int newY, vector<vector<char>>& grid, map<pair<int,int>, bool> &visit){
+        if(newX >= 0 && newY >= 0 && newX < grid.size() && newY < grid[0].size() && grid[newX][newY]=='1' && visit[{newX, newY}] == false){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void bfs(int srcX, int srcY, vector<vector<char>>& grid, map<pair<int,int>, bool> &visit){
         queue<pair<int, int>>q;
+        // initial State
+        q.push({srcX, srcY});
+        visit[{srcX, srcY}] = true;
+        while(!q.empty()){
+            pair<int,int> frontNodePair = q.front();
+            q.pop();
+            int tempX = frontNodePair.first;
+            int tempY = frontNodePair.second;
 
-        int count=0;
-        vector<pair<int, int>> directions = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j] == '1'){
+            int dx[] = {-1, 1, 0, 0};
+            int dy[] = {0, 0, 1, -1};
+            for(int i=0; i<4; i++){
+                int newX = tempX + dx[i];
+                int newY = tempY + dy[i];
+                if(isSafe(newX, newY, grid, visit)){
+                    q.push({newX, newY});
+                    visit[{newX, newY}] = true;
+                }
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // queue<pair<int, int>>q;
+        map<pair<int,int>, bool> visit;
+        int count = 0;
+        // har node ke liye loop laga dia 
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visit[{i,j}] && grid[i][j] == '1'){
+                    bfs(i, j, grid, visit);
                     count++;
-                    q.push({i, j});
-                    grid[i][j] = '0';
-
-                    while(!q.empty()){
-                        auto [x, y] = q.front();
-                        q.pop();
-
-                        for(auto [dx, dy]: directions){
-                            int newX = dx + x;
-                            int newY = dy + y;
-
-                            if(newX >=0 && newX < row && newY >=0 && newY < col && grid[newX][newY] == '1'){
-                                q.push({newX, newY});
-                                grid[newX][newY] = '0';
-                            }
-                        }
-                    }
                 }
             }
         }
