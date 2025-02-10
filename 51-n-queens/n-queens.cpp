cc3efa56dcba164a40d36f1f77294600b39ee346
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-02-10 14:51:14 +0530

    Time: 192 ms (5.26%) | Memory: 79.3 MB (6.17%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    unordered_map<int, bool>rowCheck;
+    unordered_map<int, bool>topDiagonal;
+    unordered_map<int, bool>bottomDiagonal;
+    void storeSolution(vector<vector<char>> &board, vector<vector<string>> &ans){
+        vector<string>tempSol;
+        for(auto vec:board){
+            string temp = "";
+            for(auto ch:vec){
+                temp.push_back(ch);
+            }
+            tempSol.push_back(temp);
+        }
+        ans.push_back(tempSol);
+    }
+    bool isSafe(int row, int col, vector<vector<char>> board){
+        if(rowCheck[row] != true && topDiagonal[col-row] != true && bottomDiagonal[row+col] != true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void solve(vector<vector<char>> &board, vector<vector<string>> &ans, int n, int col){
+        // base case
+        if(col >= n){
+            // sare column process ho gaye hai we have a solution to store
+            storeSolution(board, ans);
+            return;
+        }
+        // main login
+        // is column ke har row pr queen place kr ke dekkho
+        for(int row=0; row<n; row++){
+            if(isSafe(row, col, board)){
+                // queen place kardo
+                board[row][col] = 'Q';
+                // mark all three direction that queue is flaced here
+                rowCheck[row] = true;
+                topDiagonal[col-row] = true;
+                bottomDiagonal[row+col]  = true;
+
+                // recurssion
+                solve(board, ans, n, col+1);
+                // backTracking
+                board[row][col] = '.';
+                rowCheck[row] = false;
+                topDiagonal[col-row] = false;
+                bottomDiagonal[row+col]  = false;
+            }
+        }
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        int col = 0;
+        solve(board, ans, n, col);
+        return ans;
+    }
+};
