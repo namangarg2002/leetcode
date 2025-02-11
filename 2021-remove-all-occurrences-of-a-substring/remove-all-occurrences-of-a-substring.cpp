148b496f2b01519c72d77cb4bc4bf9f8dfd57d5b
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-02-11 21:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (90.62%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,33 +1,9 @@
 class Solution {
 public:
-
-    void removeOccurenceRecursively(string &s, string &part){
-        int found = s.find(part);
-        if(found != string::npos){
-            //part string has been located.
-            // please remove it.
-            string left_part = s.substr(0, found);
-            string right_part = s.substr(found+part.size(), s.size());
-            s = left_part + right_part; 
-            removeOccurenceRecursively(s, part);
-        }
-        else{
-            // base case
-            // all the occurences of part has been removed from s
-            return;
-        }
-    }
     string removeOccurrences(string s, string part) {
-
-    //   while(s.find(part)!=string::npos){
-    //     //   if inside loop, it means that part exists in a s(main) string
-    //     s.erase(s.find(part), part.length());
-    //   }
-    //   return s;
-    // }
-
-        removeOccurenceRecursively(s, part);
+        while (s.find(part) != std::string::npos) {
+            s.erase(s.find(part), part.size());
+        }
         return s;
     }
-
 };
