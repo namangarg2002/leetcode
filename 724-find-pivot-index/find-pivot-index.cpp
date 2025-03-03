6c830204644a9376338cd0cc72bc42234a799920
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-03-03 19:39:57 +0530

    Time: 1 ms (29.81%) | Memory: 36.9 MB (15.44%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++){
-            int lsum = 0;
-            int rsum = 0;
-            for(int j=0; j<i; j++){
-                lsum += nums[j];
-            }
-            for(int j=i+1; j<nums.size(); j++){
-                rsum += nums[j];
-            }
+        // for(int i=0; i<nums.size(); i++){
+        //     int lsum = 0;
+        //     int rsum = 0;
+        //     for(int j=0; j<i; j++){
+        //         lsum += nums[j];
+        //     }
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         rsum += nums[j];
+        //     }
 
-            if(lsum==rsum){
+        //     if(lsum==rsum){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // optimised Approach
+        int n = nums.size();
+        vector<int>lsum(n),rsum(n);
+
+        lsum[0] = 0;
+        rsum[n-1] = 0;
+
+        for(int i=1; i<n; i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+        for(int i=n-2; i>=0; i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+
+        for(int i=0; i<n; i++){
+            if(lsum[i]==rsum[i]){
                 return i;
             }
         }
         return -1;
+
     }
 };
