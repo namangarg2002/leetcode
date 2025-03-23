012fa37e0ebe5819c042fb4c37780dcd1f9917f6
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-03-23 16:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (19.67%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 1, j = 0;
+        while(i<nums.size()){
+            if(nums[i]==nums[j]){
+                i++;
+            }else{
+                j++;
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+        return j+1;
+    }
+};
