175070ff1c2482222b0d23dc9b3584065913fbe8
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-03-30 13:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.59%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int findPivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int s = 0, e = n - 1;
+        
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (mid < n - 1 && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (nums[s] > nums[mid]) {
+                e = mid - 1; 
+            } else {
+                s = mid + 1;  
+            }
+        }
+
+        return -1;
+    }
+
+    int binarySearch(vector<int>& nums, int s, int e, int target) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        
+        int pivotIndex = findPivotIndex(nums);
+
+        if (pivotIndex == -1) {
+            return binarySearch(nums, 0, n - 1, target);
+        }
+
+        if (target >= nums[0] && target <= nums[pivotIndex]) {
+            return binarySearch(nums, 0, pivotIndex, target);
+        } else {
+            return binarySearch(nums, pivotIndex + 1, n - 1, target);
+        }
+    }
+};
