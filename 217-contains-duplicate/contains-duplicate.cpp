d24c313a2836f8dab30678ac519d50e7e24dd213
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-01 11:39:09 +0530

    Time: 23 ms (82.14%) | Memory: 73.5 MB (50.61%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>set;
+        for(auto &num: nums){
+            if(set.count(num)) return true;
+
+            set.insert(num);
+        }
+        return false;
+    }
+};
