3316015b3e803455179889897e5459e5fe77a6ff
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-01 14:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.72%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,18 +3,18 @@ public:
     int search(vector<int>& nums, int target) {
         int s = 0;
         int e = nums.size()-1;
-
-        int mid = s + (e-s)/2;
         while(s<=e){
+            int mid = s + (e-s)/2;
             if(nums[mid] == target){
                 return mid;
-            }else if(target < nums[mid]){
-                e = mid - 1;
-            }else{
-                s = mid + 1;
             }
-            mid = s + (e-s)/2;
+            else if(nums[mid] < target){
+                s = mid + 1;
+            }else{
+                e = mid - 1;
+            }
         }
         return -1;
+
     }
 };
