1598ba20786032c461fdd345e2add93ab64df903
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-02 14:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (99.97%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     static string str;
-    static bool compare(char ch1, char ch2){
-        return (str.find(ch1)<str.find(ch2));
+
+    static bool compare(char ch1, char ch2) {
+        return str.find(ch1) < str.find(ch2);
     }
+
     string customSortString(string order, string s) {
-        
         str = order;
         sort(s.begin(), s.end(), compare);
         return s;
