d85196f7b8086feb064eab92baf0fa96e8252489
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-02 11:47:33 +0530

    Time: 2 ms (55.45%) | Memory: 9.4 MB (84.08%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while (s.find(part) != std::string::npos) {
-            s.erase(s.find(part), part.size());
+        while(s.find(part) != string::npos){
+            s.erase(s.find(part), part.length());
         }
         return s;
     }
