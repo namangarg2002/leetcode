62f239d0960dfed68aa27460159f9e556383f65b
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-03 12:14:41 +0530

    Time: 1 ms (48.89%) | Memory: 10.3 MB (28.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp = "";
-        for(auto&ch: s){
-            if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch >= '0' && ch <= '9')){
-                temp.push_back(tolower(ch));
-            }else{
-                continue;
+        string tempStr = "";
+        for(auto& ch: s){
+            if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')) {
+                tempStr.push_back(tolower(ch));
             }
         }
 
         int i = 0;
-        int j = temp.size()-1;
+        int j = tempStr.length()-1;
         while(i<=j){
-            if(temp[i]==temp[j]){
+            if(tempStr[i] == tempStr[j]){
                 i++;
                 j--;
             }else{
                 return false;
             }
         }
+
+
         return true;
     }
 };
