76b877cb0f807dd5e982587849ead58aaada0bd3
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-03 13:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (36.45%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,82 +1,14 @@
-class TrieNode
-{
-public:
-  char value;
-  TrieNode *children[26];
-  bool isTerminal;
-  int childCount;
-
-  TrieNode(char val)
-  {
-    this->value = val;
-    for (int i = 0; i < 26; i++)
-    {
-      children[i] = NULL;
-    }
-    this->isTerminal = false;
-    this->childCount = 0;
-  }
-};
 class Solution {
 public:
-    void insertWord(TrieNode *root, string word, int index = 0)
-    {
-        if (index == word.length())
-        {
-            root->isTerminal = true;
-            return;
-        }
-        char ch = word[index];
-        int childindex = ch - 'a';
-        TrieNode *child;
-        if (root->children[childindex] != NULL)
-        {
-            // child present
-            child = root->children[childindex];
-        }
-        else
-        {
-            // absent
-            child = new TrieNode(ch);
-            root->children[childindex] = child;
-            root->childCount++;
-        }
-        // recurssion will handle further
-        insertWord(child, word, index + 1);
-    }
-
-    void findLCP(string &ans, TrieNode* root){
-        // base case
-        if(root->isTerminal == true){
-            return;
-        }
-
-        TrieNode* child;
-        // abhi root node par hu
-        if(root->childCount == 1){
-            // child tak jaao
-            for(int i=0; i<26; i++){
-                if(root->children[i] != NULL){
-                    child = root->children[i];
-                }
-            }
-            // ans mai store karwao
-            ans.push_back(child->value);
-        }else{
-            return;
-        }
-        // rest recurssion will handle it
-        findLCP(ans, child);
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        TrieNode* root = new TrieNode('-');
-        // insert all strings
-        for(auto str: strs){
-            insertWord(root, str);
+        if(strs.empty()) return "";
+        string prefix = strs[0];
+        for(int i=1; i<strs.size(); i++){
+            while(strs[i].find(prefix) != 0){
+                prefix = prefix.substr(0, prefix.size()-1);
+                if(prefix.empty()) return "";
+            }
         }
-        string ans = "";
-        // string str = strs[0];
-        findLCP(ans, root);
-        return ans;
+        return prefix;
     }
 };
