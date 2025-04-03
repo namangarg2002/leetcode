e6ff6e5cbc46203bdea5975be0de8672312ff04c
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-03 12:25:42 +0530

    Time: 9 ms (8.78%) | Memory: 9.7 MB (48.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,17 +4,17 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        int i = 0;
-        int j = 0;
-        while(i<s.size() || j<t.size()){
-            if(s[i] == t[j]){
-                i++;
-                j++;
-            }else{
-                return false;
-            }
-        }
+        // int i = 0;
+        // int j = 0;
+        // while(i<s.size() || j<t.size()){
+        //     if(s[i] == t[j]){
+        //         i++;
+        //         j++;
+        //     }else{
+        //         return false;
+        //     }
+        // }
 
-        return true;
+        return (s==t) ? true : false;
     }
 };
