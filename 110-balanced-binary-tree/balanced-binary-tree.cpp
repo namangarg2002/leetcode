c6beb67fca625a085d94f7d133885272576e2ab5
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-04-07 18:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (84.44%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,56 +11,19 @@
  */
 class Solution {
 public:
-    // int height(TreeNode* root){
-    //     if(root == NULL){
-    //        return true; 
-    //     }
-    //     int leftHeight = height(root->left);
-    //     int rightHeight = height(root->right);
-    //     int finalAns = max(leftHeight, rightHeight) + 1;
-    //     return finalAns;
-    // }
-    // bool isBalanced(TreeNode* root) {
-    //     // base case
-    //     if(root == NULL){
-    //         return true;
-    //     }
-
-    //     // currNode = solve
-    //     int leftHeight = height(root->left);
-    //     int rightHeight = height(root->right);
-
-    //     int diff = abs(leftHeight-rightHeight);
-
-    //     bool currNodeAns = (diff <= 1);
-
-    //     int leftAns = isBalanced(root->left);
-    //     int rightAns = isBalanced(root->right);
-
-    //     if(currNodeAns && leftAns && rightAns){
-    //         return true;
-    //     }
-    //     else{
-    //         return false;
-    //     }
-    // }
-    bool isbalanced = true;
     int height(TreeNode* root){
         if(!root) return 0;
 
-        int left = height(root->left);
-        int right = height(root->right);
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
 
-        //check for current Node, is balanced or not
-        if(isbalanced && abs(left - right)>1){
-            isbalanced = false;
+        if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1){
+            return -1;
+        }else{
+            return 1 + max(leftHeight, rightHeight);
         }
-
-        return 1 + max(left, right);
     }
-    bool isBalanced(TreeNode* root){
-        height(root);
-        return isbalanced;
+    bool isBalanced(TreeNode* root) {
+        return (height(root) != -1) ? true : false;
     }
-
 };
