f7617775cc35c6a4036e4fed90e455b9f571acfb
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-04-07 19:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.97%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,51 +1,40 @@
 class Solution {
 public:
-    int firstOccurence(vector<int>& nums, int n, int target){
+    int firstOccurence(vector<int>& nums, int target, int n){
         int s = 0;
-        int e = n-1;
-        int mid = s + (e-s)/2;
-        int ans = -1;
+        int e = n - 1;
+        int first = -1;
         while(s<=e){
+            int mid = s + (e-s)/2;
             if(nums[mid] == target){
-                ans = mid;
+                first = mid;
                 e = mid - 1;
-            }else if(target > nums[mid]){
+            }else if(nums[mid] < target){
                 s = mid + 1;
             }else{
                 e = mid - 1;
             }
-            mid = s + (e-s)/2; 
         }
-        return ans;
+        return first;
     }
-    int lastOccurence(vector<int>& nums, int n, int target){
+    int lastOccurence(vector<int>& nums, int target, int n){
         int s = 0;
-        int e = n-1;
-        int mid = s + (e-s)/2;
-        int ans = -1;
+        int e = n - 1;
+        int last = -1;
         while(s<=e){
+            int mid = s + (e-s)/2;
             if(nums[mid] == target){
-                ans = mid;
+                last = mid;
                 s = mid + 1;
-            }else if(target > nums[mid]){
+            }else if(nums[mid] < target){
                 s = mid + 1;
             }else{
                 e = mid - 1;
             }
-            mid = s + (e-s)/2;
         }
-        return ans;
+        return last;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int>ans;
-        int firstPositionIndex = firstOccurence(nums, n, target);
-        int lastPositionIndex = lastOccurence(nums, n, target);
-
-        ans.push_back(firstPositionIndex);
-        ans.push_back(lastPositionIndex);
-
-        return ans;
-        
+        return {firstOccurence(nums, target, nums.size()), lastOccurence(nums, target, nums.size())};
     }
 };
