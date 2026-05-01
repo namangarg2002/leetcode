2ab0f2839ed48a88f2827acb8290ba964aa0d13f
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-05-01 17:02:53 +0530

    Time: 60 ms (37.35%) | Memory: 24.1 MB (24.40%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,15 +8,41 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* a = headA;
-        ListNode* b = headB;
-
+    int getLength(ListNode* &head){
+        ListNode* temp = head;
+        int count = 0;
+        while(temp != NULL){
+            temp = temp->next;
+            count++;
+        }
+        return count;
+    }
+    ListNode* traversalLL(ListNode* &a, ListNode* &b){
         while(a != b){
-            a = a ? a->next : headB;
-            b = b ? b->next : headA;
+            a = a->next;
+            b = b->next;
+        }
+        return a;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int List1Lenght = getLength(headA);
+        int List2Lenght = getLength(headB);
+
+        if(List2Lenght > List1Lenght){
+            int diff = List2Lenght - List1Lenght;
+            for(int i=0; i<diff; i++){
+                headB = headB->next;
+            }
+
+            return traversalLL(headA, headB);
+        }else{
+            int diff = List1Lenght - List2Lenght;
+            for(int i=0; i<diff; i++){
+                headA = headA->next;
+            }
+
+            return traversalLL(headA, headB);
         }
 
-        return b;
     }
 };
