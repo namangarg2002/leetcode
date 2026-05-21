29cd07a53601959bd30f73871649a5df43545e5d
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-05-21 12:31:56 +0530

    Time: 43 ms (39.46%) | Memory: 75.7 MB (17.24%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,9 +10,9 @@
  */
 class Solution {
 public:
-    ListNode*findMid(ListNode* head){
-        ListNode*slow = head;
-        ListNode*fast = head;
+    ListNode* findMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
         while(fast->next != NULL){
             fast = fast->next;
             if(fast->next != NULL){
@@ -22,13 +22,12 @@ public:
         }
         return slow;
     }
-    ListNode*merge(ListNode* left, ListNode* right){
+    ListNode* merge(ListNode* left, ListNode* right){
         if(left == NULL) return right;
         if(right == NULL) return left;
 
-        ListNode*ans = new ListNode(-1);
-        ListNode*mptr = ans;
-
+        ListNode* ans = new ListNode(-1);
+        ListNode* mptr = ans;
         while(left && right){
             if(left->val <= right->val){
                 mptr->next = left;
@@ -39,32 +38,30 @@ public:
                 mptr = right;
                 right = right->next;
             }
-        }
-        if(left){
-            mptr->next = left;
-        }else{
-            mptr->next = right;
+
+            if(left){
+                mptr->next = left;
+            }else{
+                mptr->next = right;
+            }
         }
         return ans->next;
     }
-
     ListNode* sortList(ListNode* head) {
         if(head == NULL || head->next == NULL){
             return head;
         }
-        // Break LL intot two halves using mid node;
-        ListNode*mid = findMid(head); 
 
-        ListNode*left = head;
-        ListNode*right = mid->next;
-        mid->next = NULL; // for making two differnrt LL i.e., Left and Right
+        ListNode* mid = findMid(head);
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
 
-        // sort RE
         left = sortList(left);
         right = sortList(right);
 
-        // merge both Left and Right LL
-        ListNode*mergedLL = merge(left, right);
-        return mergedLL;
+        ListNode* mergeList = merge(left, right);
+
+        return mergeList;
     }
 };
